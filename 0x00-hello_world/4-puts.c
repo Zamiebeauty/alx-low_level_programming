13b6a1542ef7fe8f9c60ae61f38652596e373c48
Zamiebeauty <balogunolamide1806@gmail.com>
2022-12-11 08:30:55 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry Point
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
-
 int main(void)
 {
-	puts("\"Programming is like a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
