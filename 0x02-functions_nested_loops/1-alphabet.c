74e1f07667295c872785d768e631cfc12fa82863
Zamiebeauty <balogunolamide1806@gmail.com>
2022-12-14 04:25:10 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * Print_alphabet - Prints the alpha in lowercase
+ */
+void Print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
